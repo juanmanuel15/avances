932a6980add5fdbd9052224e86977ea44ca5814a
Juan1508 <juanmanuelhc15@outlook.com>
2018-07-05 09:04:04 -0500

    Se agregaron todos los archivos

---
+++ admin.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+
+
+	<link rel="stylesheet"  href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/main.css">
+	<link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Ubuntu:400,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
+
+	<title>admin</title>
+</head>
+<body>
+	<nav class="se-VerdeClaro padding-largo text-center" >
+		
+		<ul class="no-lista negrita fuente-indie color-barra">
+			<a href="everfail.php"><li class="col-md-3 line-block">Inicio</li></a>
+			<a href="compras.php"><li class="col-md-3 line-block">Compras</li></a>
+			<a href="factura.php"><li class="col-md-3 line-block">Factura</li></a>
+			<a href="admin.php"><li class="col-md-3 line-block">Administración</li></a>		
+
+		</ul>
+
+	</nav>
+
+	<section>
+		<div class="text-center fuente-indie">
+			<h1 class=""> Administración </h1>			
+		</div>
+		
+	</section>
+	
+
+
+</body>
+</html>
